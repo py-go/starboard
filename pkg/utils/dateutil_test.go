ae96e80df893d340d50155283d78d65da6e27a65
Daniel Pacak <pacak.daniel@gmail.com>
2022-08-18 20:30:33 +0200

    chore: remove gorhill/cronexpr dependency
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/utils/dateutil_test.go
+++ pkg/utils/dateutil_test.go
@@ -1,47 +1,13 @@
 package utils
 
 import (
-	"github.com/aquasecurity/starboard/pkg/ext"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/aquasecurity/starboard/pkg/ext"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNextCronDuration(t *testing.T) {
-	tests := []struct {
-		name         string
-		cron         string
-		creationTime string
-		wantDuration int
-		wantError    string
-	}{
-		{name: "good cron", cron: "* * * * *", creationTime: "2050-11-12T11:00:00", wantDuration: 251749},
-		{name: "bad cron", cron: "* *", creationTime: "2050-11-12T11:00:00", wantError: "missing field(s)"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tm, err := parseTime(tt.creationTime)
-			if err != nil {
-				t.Errorf(err.Error())
-			}
-			duration, err := NextCronDuration(tt.cron, tm, ext.NewSystemClock())
-			if err != nil {
-				assert.Equal(t, err.Error(), tt.wantError)
-			}
-			if err == nil {
-				assert.True(t, !(int(duration.Hours()) > tt.wantDuration))
-			}
-		})
-	}
-}
-func parseTime(creationTime string) (time.Time, error) {
-	layout := "2006-01-02T15:04:05"
-	tm, err := time.Parse(layout, creationTime)
-	if err != nil {
-		return ext.NewSystemClock().Now(), err
-	}
-	return tm, nil
-}
 func TestDurationExceeded(t *testing.T) {
 	tests := []struct {
 		name     string
